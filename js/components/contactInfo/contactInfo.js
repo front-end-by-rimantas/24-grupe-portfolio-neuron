c0e40c8fa0a6f5a65cbd7e85fc520c9336141ac2
zvlzvl <76951647+zvlzvl@users.noreply.github.com>
2021-02-25 13:42:37 +0200

    contactInfo test

--- js/components/contactInfo/contactInfo.js
+++ js/components/contactInfo/contactInfo.js
@@ -4,7 +4,24 @@ function renderContactInfo(data) {
         Array.isArray(data)) {
         return 'Netinkamo tipo parametras'
    }
+   const allowedKeys = ['selector', 'list'];
+   const keys = Object.keys(data);
+   if(keys.length !== 2) {
+       return 'Netinkamas kiekis objektu raktazodziu'
+   }
+   for (const key of keys) {
+      if(!allowedKeys.includes(key)) {
+         return `Neatpazintas raktazodis "${key}"`;
+   }
+}
 
+if (!Array.isArray(data.list)) {
+   return 'Parametras "list" netinkamo formato'
+}
+if (typeof data.selector !== 'string' || data.selector === ''){
+   return 'Parametras "selector" netinkamo formato'
+}
+   
    // logic
    // post logic validation
    // result return
--- js/components/contactInfo/contactInfo.test.js
+++ js/components/contactInfo/contactInfo.test.js
@@ -1,17 +1,60 @@
 import { TestScheduler } from 'jest';
 import { renderContactInfo } from './contactInfo.js';
 
-describe('Pagrindinė contactInfo duomenu struktura', () => {
+describe('ar tinkamas contactInfo duomenu tipas', () => {
     test('nepriima skaiciaus', () => {
         expect(renderContactInfo(5)).toBe('Netinkamo tipo parametras');
-    })
-    test('nepriima array', () => {
-        expect(renderContactInfo([])).toBe('Netinkamo tipo parametras');
     })
     test('nepriima teksto', () => {
         expect(renderContactInfo('labas')).toBe('Netinkamo tipo parametras');
     })
-    test('priima objekta', () => {
-        expect(renderContactInfo({})).toBeTruthy();
+    test('nepriima array', () => {
+        expect(renderContactInfo([])).toBe('Netinkamo tipo parametras');
+    })
+})
+
+describe('Tikriname, ar objekto struktura yra tinkama', () => {
+    test('tuscias objektas', () => {
+        expect(renderContactInfo({})).toBe('Netinkamas kiekis objektu raktazodziu')
+    })
+    test('neuztenka, kai tik vienas raktazodis', () => {
+        expect(renderContactInfo({selector: '#demo'})).toBe('Netinkamas kiekis objektu raktazodziu')
+    })
+    test('netinkamas raktazodziu rinkinys, prigavo "size"', () => {
+        expect(renderContactInfo({selector: '#demo', size: 5})).toBe('Neatpazintas raktazodis "size"')
+    })
+    test('netinkamas raktazodziu rinkinys, prigavo "select"', () => {
+        expect(renderContactInfo({select: '#demo', size: 5})).toBe('Neatpazintas raktazodis "select"')
+    })
+    test('netinkamas raktazodziu rinkinys, "prigavo size"', () => {
+        expect(renderContactInfo({size: 5, select: '#demo'})).toBe('Neatpazintas raktazodis "size"')
+    })
+    test('"list" negali buti skaičius', () => {
+        expect(renderContactInfo({selector: '#demo', list: 5})).toBe('Parametras "list" netinkamo formato')
+    })
+    test('"list" negali buti tekstas', () => {
+        expect(renderContactInfo({selector: '#demo', list: 'labas'})).toBe('Parametras "list" netinkamo formato')
+    })
+    test('"selector" negali buti skaicius', () => {
+        expect(renderContactInfo({selector: 5, list: [] })).toBe('Parametras "selector" netinkamo formato')
+    })
+    test('"selector" negali buti skaicius', () => {
+        expect(renderContactInfo({selector: '', list: [] })).toBe('Parametras "selector" netinkamo formato')
+    })
+})
+
+describe('Tinkami duomenys grazina teisinga "contactInfo" html', () => {
+    test('Turi grazinti "true", jei parametrai yra tinkami', () => {
+        const params = {
+            selector: '#contactInfo_block',
+            list: [
+                {
+                    icon: 'flaticon-placeholder',
+                    title: 'Visit us',
+                    text: '530 Old BUffalo Street <br> Northwest #205, New York - 3087'
+                }
+            ]
+        }
+        expect(renderContactInfo(params)).toBeTruthy();
     })
 })
